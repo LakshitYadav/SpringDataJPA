9165c3b2bec8d9d1ce65f7aea1b1a6b95c2abc49
Lakshit Yadav <59692570+LakshitYadav@users.noreply.github.com>
2021-02-04 16:27:53 +0530

    Add files via upload

---
+++ movie-cruiser-spring/sql/moviecruiser-data.sql
@@ -0,0 +1,19 @@
+insert into Movies (movie_title, movie_box_office, movie_active, movie_date_of_launch, movie_genre, movie_teaser) values (
+"Gifted", 7.6, true, "2017-04-12", "Drama", "https://www.imdb.com/video/vi3537025049?playlistId=tt4481414&ref_=tt_ov_vi");
+insert into Movies (movie_title, movie_box_office, movie_active, movie_date_of_launch, movie_genre, movie_teaser) values (
+"Interstellar", 8.6, true, "2014-11-07", "Adventure", "https://www.imdb.com/video/vi1586278169?playlistId=tt0816692&ref_=tt_ov_vi");
+insert into Movies (movie_title, movie_box_office, movie_active, movie_date_of_launch, movie_genre, movie_teaser) values (
+"The Game Plan", 6.1, true, "2017-08-21", "Comedy", "https://www.imdb.com/video/vi2649621529?playlistId=tt0492956&ref_=tt_ov_vi");
+insert into Movies (movie_title, movie_box_office, movie_active, movie_date_of_launch, movie_genre, movie_teaser) values (
+"The Avengers", 8.4, false, "2017-07-02", "Superhero", "https://www.imdb.com/video/vi528070681?playlistId=tt4154756&ref_=tt_ov_vi");
+insert into Movies (movie_title, movie_box_office, movie_active, movie_date_of_launch, movie_genre, movie_teaser) values (
+"Shutter Island", 8.2, true, "2022-11-02", "Thriller", "https://www.imdb.com/video/vi4064281?playlistId=tt1130884&ref_=tt_ov_vi");
+insert into Users (user_first_name, user_last_name) values ("Arya","Stark");
+insert into Users (user_first_name, user_last_name) values ("John","Snow");
+
+insert into Favorites values(1,1);
+insert into Favorites values(1,2);
+insert into Favorites values(1,3);
+insert into Favorites values(2,1);
+insert into Favorites values(2,2);
+insert into Favorites values(2,3);
